370978d2bea03fe31ece59772272335fc792ccca
Denys Astafiev <45298493+DenAst17@users.noreply.github.com>
2023-01-16 18:11:06 +0200

    process.env for netlify

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_API_KEY,
-  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_APP_ID,
+  apiKey: import.meta.env.VITE_API_KEY || process.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN || process.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID || process.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET || process.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID || process.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID || process.env.VITE_APP_ID,
 };
 
 export const firebase = initializeApp(firebaseConfig);
